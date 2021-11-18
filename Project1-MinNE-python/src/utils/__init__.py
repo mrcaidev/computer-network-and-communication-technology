c6b2c7b0a6d15cc0b7665dc0319b34897216ac68
MrCai <1014305148@qq.com>
2021-11-18 21:45:07 +0800

    :truck: 重命名

--- Project1-MinNE-python/src/utils/__init__.py
+++ Project1-MinNE-python/src/utils/__init__.py
@@ -1,5 +1,5 @@
 from utils.coding import *
 from utils.event import *
 from utils.frame import Frame
+from utils.layer import *
 from utils.param import Constant as const
-from utils.sockets import *
