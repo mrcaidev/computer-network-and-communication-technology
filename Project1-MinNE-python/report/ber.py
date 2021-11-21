8bb4fa04c9abaa3b1370a33213473a5dc1cf9148
MrCai <1014305148@qq.com>
2021-11-21 17:29:39 +0800

    :pencil: 更新阶段三吞吐量

--- Project1-MinNE-python/report/ber.py
+++ Project1-MinNE-python/report/ber.py
@@ -34,44 +34,63 @@ recv_speed_1 = np.array(
         [823.0, 835.6, 2481.9],
     ]
 )
-# send_speed_2 = np.array(
-#     [802.3, 799.4, 660.9, 496.6, 392.7, 380.4, 360.6, 345.9, 283.3, 190.4, 120.6]
-# )
-# recv_speed_2 = np.array(
-#     [842.4, 840.2, 687.0, 512.8, 402.2, 389.4, 372.8, 353.6, 301.2, 210.5, 134.4]
-# )
+send_speed_2 = np.array(
+    [
+        [2642.8, 2874.0, 2925.1],
+        [2600.5, 2273.9, 2560.4],
+        [1799.9, 1741.9, 1761.4],
+        [2521.0, 1904.5, 1437.0],
+        [1436.8, 1605.0, 1099.0],
+        [1310.3, 1800.0, 1883.1],
+        [702.9, 802.8, 1621.7],
+        [1289.7, 1017.6, 1671.4],
+        [2481.5, 608.9, 768.9],
+        [806.8, 1169.9, 1462.6],
+        [562.8, 946.6, 1099.1],
+    ]
+)
+recv_speed_2 = np.array(
+    [
+        [2925.3, 3343.4, 3412.6],
+        [2874.7, 2480.4, 2874.2],
+        [1949.6, 1860.8, 1927.4],
+        [2874.2, 2047.0, 1545.2],
+        [1530.7, 1741.5, 3485.8],
+        [1388.0, 1973.6, 2073.8],
+        [1269.8, 1621.2, 1742.4],
+        [1353.5, 1070.8, 1799.7],
+        [2823.1, 980.7, 795.1],
+        [1653.9, 1231.5, 1574.9],
+        [576.7, 980.7, 1153.5],
+    ]
+)
 
 send_speed_1_average = np.array([np.mean(lst) for lst in send_speed_1])
 recv_speed_1_average = np.array([np.mean(lst) for lst in recv_speed_1])
-plt.title("BER's Impact on Sending/Receiving Speed")
+send_speed_2_average = np.array([np.mean(lst) for lst in send_speed_2])
+recv_speed_2_average = np.array([np.mean(lst) for lst in recv_speed_2])
+
+fig, axes = plt.subplots(1, 2)
+
+plt.subplot(1, 2, 1)
+plt.title("No Switch")
 plt.xlim(0, 1)
 plt.ylim(0, 5000)
 plt.plot(ber, send_speed_1_average, label="Send")
 plt.plot(ber, recv_speed_1_average, label="Receive")
 plt.legend()
-plt.grid(True)
 
-# fig, axes = plt.subplots(1, 2)
+plt.subplot(1, 2, 2)
+plt.title("With Switch")
+plt.xlim(0, 1)
+plt.ylim(0, 5000)
+plt.plot(ber, send_speed_2_average, label="Send")
+plt.plot(ber, recv_speed_2_average, label="Receive")
+plt.legend()
 
-# plt.subplot(1, 2, 1)
-# plt.title("No Switch")
-# plt.xlim(0, 1)
-# plt.ylim(0, 2000)
-# plt.plot(ber, send_speed_1, label="Send")
-# plt.plot(ber, recv_speed_1, label="Receive")
-# plt.legend()
-
-# plt.subplot(1, 2, 2)
-# plt.title("With Switch")
-# plt.xlim(0, 1)
-# plt.ylim(0, 2000)
-# plt.plot(ber, send_speed_2, label="Send")
-# plt.plot(ber, recv_speed_2, label="Receive")
-# plt.legend()
-
-# fig.add_subplot(111, frameon=False)
-# plt.tick_params(labelcolor="none", top=False, bottom=False, left=False, right=False)
-# plt.suptitle("BER's Impact on Sending/Receiving Speed")
+fig.add_subplot(111, frameon=False)
+plt.tick_params(labelcolor="none", top=False, bottom=False, left=False, right=False)
+plt.suptitle("BER's Impact on Sending/Receiving Speed")
 plt.xlabel("Bit Error Rate (%)")
 plt.ylabel("Speed (bps)")
 plt.show()
