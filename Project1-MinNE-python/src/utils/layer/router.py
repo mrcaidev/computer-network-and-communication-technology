255f536a6ef88f10e62dba8a4c3a48a49a6a3af1
MrCai <1014305148@qq.com>
2021-11-22 19:41:05 +0800

    :sparkles: 路由表编写完成

--- Project1-MinNE-python/src/utils/layer/router.py
+++ Project1-MinNE-python/src/utils/layer/router.py
@@ -1,173 +1,195 @@
-from collections import namedtuple
-from select import select
-
-import utils.constant as const
-from utils.coding import bits_to_string, string_to_bits
-from utils.layer._abstract import AbstractLayer
+from dataclasses import dataclass
+from json import loads
 
 
-class RouterTable(dict):
-    Path = namedtuple("Path", ["next", "exit", "cost"])
+@dataclass
+class Path:
+    """路由表内存储的，到达目的地的路径。"""
 
-    def __init__(self) -> None:
-        """初始化内部字典。"""
-        return super().__init__()
+    # 路线的下一跳。
+    next: str
+    # 到达下一跳的本地物理层端口。
+    exit: str
+    # 到达目的地的费用。
+    cost: int
+    # 当前是否已经最优化。
+    optimized: bool
+
+
+class RouterTable(dict[str, Path]):
+    """
+    路由表，内部是字典。
+
+    - 键：目的路由器网络层端口号。
+    - 值：到达目的路由器的最佳路径。
+    """
+
+    def __init__(self, port: str) -> None:
+        """初始化路由表。"""
+        super().__init__()
+        self._port = port
 
     def __str__(self) -> str:
         """打印路由表。"""
-        head = """------------------------------------------------
-| Destination | Next router | Exit port | Cost |
-|-------------|-------------|-----------|------|"""
+        head = f"{'-'*46}\n| Destination | Next jump | Exit port | Cost |\n|{'-'*13}|{'-'*11}|{'-'*11}|{'-'*6}|"
         body = "\n".join(
             [
-                f"|{dst.center(13)}|{next.center(13)}|{exit.center(11)}|{str(cost).center(6)}|"
-                for dst, (next, exit, cost) in self.items()
+                f"|{dst.center(13)}|{path.next.center(11)}|{path.exit.center(11)}|{str(path.cost).center(6)}|"
+                for dst, path in filter(
+                    lambda item: item[0] != self._port, self.items()
+                )
             ]
         )
-        return f"{head}\n{body}\n{'-' * 48}"
+        return f"{head}\n{body}\n{'-'*46}"
 
-    def initialize(self, initializer: dict[str, list[str, str, int]]) -> None:
+    def initialize(self, initializer: dict[str, dict]) -> str:
         """
-        初始化路由表。
+        从JSON文件初始化路由表周围环境。
 
         Args:
-            initializer: 用于初始化的字典，键值对格式如下：
-            - 键：目的应用层端口号。
-            - 值：一个三元列表。
-                - [0] 下一跳的路由器网络层端口号。
-                - [1] 为了到达下一跳，应该从哪个本地物理层端口送出。
-                - [2] 到达这个目的应用层的费用。
+            initializer: 用于初始化环境的字典，键值对格式如下：
+            - 键：相邻路由器网络层端口号。
+            - 值：一个字典，包括下列两个键：
+                - exit: 要到达该路由器，要走哪个本地物理层端口。
+                - cost: 到达该路由器的费用。
+
+        Returns:
+            首先应合并谁的路由表。
         """
-        for dst, [next, exit, cost] in initializer.items():
-            self[dst] = RouterTable.Path(next=next, exit=exit, cost=cost)
+        self[self._port] = Path(next="", exit="", cost=0, optimized=True)
+        min_cost, min_dst = float("inf"), ""
+        for dst, dic in initializer.items():
+            cur_cost = dic["cost"]
+            self[dst] = Path(next=dst, exit=dic["exit"], cost=cur_cost, optimized=False)
+            if cur_cost < min_cost:
+                min_cost = cur_cost
+                min_dst = dst
+        return min_dst
 
     def pack(self) -> str:
         """
-        将路由表打包成字符串。
+        将路由表内的下一跳与费用打包成字符串。
 
         Returns:
-            一个字符串，格式为|目的端口:费用|目的端口:费用|...|
+            包含路由表信息的字符串，路径间以"|"分隔，路径内以":"分隔。
         """
-        return "|".join([f"{dst}:{cost}" for dst, (_, _, cost) in self.items()])
+        return "|".join(
+            f"{dst}:{cost}"
+            for dst, cost in filter(
+                lambda item: item[0] != self._port,
+                [(dst, str(path.cost)) for dst, path in self.items()],
+            )
+        )
 
-    def unpack(
-        self, src_router: str, in_port: str, string: str
-    ) -> dict[str, tuple[str, str, int]]:
+    def unpack(string: str) -> dict[str, int]:
         """
         将字符串解包为路由表。
 
         Args:
-            src_router: 发来这份路由表的路由器网络层端口号。
-            in_port: 接收到这份路由表的本地物理层端口号。
-            string: 要解包的字符串。
+            string: 包含路由表信息的字符串。
 
         Returns:
-            解包所得的路由表。
+            一个字典，键值对格式如下：
+            - 键：目的路由器网络层端口号。
+            - 值：到达该路由器的费用。
         """
-        unpacked_table = {}
+        new_table = {}
+        for path in string.split("|"):
+            dst, cost = path.split(":")
+            new_table.update({dst: int(cost)})
+        return new_table
 
-        # 逐条解析为Path元组。
-        for his_path in string.split("|"):
-            dst, his_cost = his_path.split(":")
-            unpacked_table[dst] = RouterTable.Path(
-                next=src_router, exit=in_port, cost=int(his_cost) + 1
+    def merge(self, src: str, table: dict[str, int]) -> str:
+        """
+        合并外部路由表与本地路由表。
+
+        Args:
+            src: 外部路由表的来源。
+            table: 发来的路由表。
+
+        Returns:
+            接下来合并谁的路由表。
+        """
+        # 标记到达该来源的路径为最优化。
+        self[src].optimized = True
+
+        # 创建本地路由表的一份拷贝。
+        old_copy = self.copy()
+
+        min_cost, min_dst = float("inf"), ""
+        for dst, new_cost in table.items():
+            # 比对两表内，关于该目的地的数据。
+            old_path = self.get(dst, None)
+            new_path = Path(
+                next=src,
+                exit=self[src].exit,
+                cost=new_cost + self[src].cost,
+                optimized=False,
             )
-        return unpacked_table
 
-    def update(self, path_info: dict[str, tuple[str, str, int]]) -> bool:
-        """
-        更新路由表。
-
-        Args:
-            info: 一条路径的信息，键值对格式如下：
-            - 键：目的应用层端口号。
-            - 值：一个三元元组。
-                - [0] 下一跳的路由器网络层端口号。
-                - [1] 为了到达下一跳，应该从哪个本地物理层端口送出。
-                - [2] 到达这个目的应用层的费用。
-
-        Returns:
-            是否有更新，有更新为True，无更新为False。
-        """
-        updated = False
-        # 解包传入的字典。
-        for dst, new_path in path_info.items():
-            # 查找路由表内，这个目的应用层的已有路径。
-            old_path, recorded = self.search(dst)
-
-            # 如果路由表还没记录到这个应用层的路径，或者记录的路径费用较高，就更新路径。
-            if not recorded or new_path.cost < old_path.cost:
+            # 如果表里没有这条记录，就追加进表。
+            if old_path == None:
                 self[dst] = new_path
-                updated = True
+                cur_cost = new_cost
 
-        return updated
+            # 如果这条记录已经是最优化的，就跳过。
+            elif old_path.optimized:
+                continue
 
-    def search(self, dst: str) -> tuple[tuple[str, str, int], bool]:
-        """
-        在路由表中查找路径。
+            # 如果本地记录费用低，就维持本地路径。
+            elif old_path.cost < new_path.cost:
+                cur_cost = old_path.cost
 
-        Args:
-            目的应用层端口号。
+            # 如果新记录费用低，就更新路径。
+            else:
+                self[dst] = new_path
+                cur_cost = new_cost
 
-        Returns:
-            一个二元元组。
-            - [0] Path三元元组，包含到达目的应用层的路径信息。
-            - [1] 是否记录了该路径，有记录为True，无记录为False。
-        """
-        result = self.get(dst, None)
-        return result, result != None
+            # 更新当前非最优化路径集的费用最小值。
+            if min_cost > cur_cost:
+                min_cost = cur_cost
+                min_dst = dst
+
+            # 如果这条路径原来就有，就在拷贝中删掉这一条。
+            if old_path != None:
+                old_copy.pop(dst)
+
+        # 检测还有哪些端口没被比较过最小值。
+        remained = dict(filter(lambda item: not item[1].optimized, old_copy.items()))
+
+        # 如果还有端口没被比较过，就将最小值与这些端口相比较。
+        if remained != {}:
+            min_item = min(remained.items(), key=lambda item: item[1].cost)
+            if min_item[1].cost < min_cost:
+                min_dst = min_item[0]
+
+        # 返回最小值对应的端口。
+        return min_dst
 
 
-class RouterLayer(AbstractLayer, RouterTable):
-    """路由器网络层。"""
+with open("config/router_env.json", "r", encoding="utf-8") as fr:
+    router_env = loads(fr.read())
 
-    def __init__(self, port: str) -> None:
-        """初始化网络层。"""
-        AbstractLayer.__init__(self, port)
-        RouterTable.__init__(self)
+table_11200 = RouterTable("11200")
+table_21200 = RouterTable("21200")
+table_31200 = RouterTable("31200")
+table_41200 = RouterTable("41200")
+table_51200 = RouterTable("51200")
 
-    def __str__(self) -> str:
-        """打印网络层信息。"""
-        return f"<Router Layer at 127.0.0.1:{self._port}>"
+next_merge_router = table_11200.initialize(router_env["11200"])
+print(table_11200)
 
-    def send_to_phy(self, binary: str, port: str) -> int:
-        """
-        向物理层发送消息。
+table_21200.initialize(router_env["21200"])
+package_21200 = table_21200.pack()
+table_31200.initialize(router_env["31200"])
+package_31200 = table_31200.pack()
+table_41200.initialize(router_env["41200"])
+package_41200 = table_41200.pack()
+table_51200.initialize(router_env["51200"])
+package_51200 = table_51200.pack()
 
-        Args:
-            binary: 要发的消息（01序列）。
-            port: 信息要送到的本地物理层端口。
-
-        Returns:
-            总共发送的字节数。
-        """
-        return self._send(string_to_bits(binary), port)
-
-    def receive_from_phy(self) -> tuple[str, str, bool]:
-        """
-        从物理层接收消息。
-
-        Returns:
-            一个三元元组。
-            - [0] 接收到的消息。
-            - [1] 消息来自的本地物理层端口。
-            - [2] 是否接收成功，成功为True，失败为False。
-        """
-        binary, port, success = self._receive()
-        binary = bits_to_string(binary) if success else binary
-        return binary, port, success
-
-    def has_message(self) -> bool:
-        """
-        检测本层套接字是否有可读消息。
-
-        Returns:
-            可读为True，不可读为False。
-        """
-        ready_sockets, _, _ = select(
-            [self._socket], [], [], const.Network.SELECT_TIMEOUT
-        )
-        return len(ready_sockets) != 0
-
-    def print_table(self) -> None:
-        print(RouterTable.__str__(self))
+while next_merge_router != "":
+    next_merge_router = table_11200.merge(
+        next_merge_router, RouterTable.unpack(eval(f"package_{next_merge_router}"))
+    )
+    print(table_11200)
