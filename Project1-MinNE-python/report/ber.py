872d6b9a6315ceb8f4e960e0230029e3a066506f
MrCai <1014305148@qq.com>
2021-11-28 13:10:13 +0800

    :pencil: 阶段四报告与PDF

--- Project1-MinNE-python/report/ber.py
+++ Project1-MinNE-python/report/ber.py
@@ -4,7 +4,7 @@ import numpy as np
 plt.style.use(["fast"])
 
 ber = np.linspace(0, 1, 11)
-send_speed_1 = np.array(
+send_2 = np.array(
     [
         [4094.1, 4201.7, 4310.3],
         [4200.1, 4307.3, 4680.9],
@@ -19,7 +19,7 @@ send_speed_1 = np.array(
         [806.8, 810.8, 2306.9],
     ]
 )
-recv_speed_1 = np.array(
+recv_2 = np.array(
     [
         [4679.5, 4680.8, 4815.5],
         [4817.2, 4813.3, 5286.6],
@@ -34,7 +34,7 @@ recv_speed_1 = np.array(
         [823.0, 835.6, 2481.9],
     ]
 )
-send_speed_2 = np.array(
+send_3 = np.array(
     [
         [2642.8, 2874.0, 2925.1],
         [2600.5, 2273.9, 2560.4],
@@ -49,7 +49,7 @@ send_speed_2 = np.array(
         [562.8, 946.6, 1099.1],
     ]
 )
-recv_speed_2 = np.array(
+recv_3 = np.array(
     [
         [2925.3, 3343.4, 3412.6],
         [2874.7, 2480.4, 2874.2],
@@ -64,28 +64,68 @@ recv_speed_2 = np.array(
         [576.7, 980.7, 1153.5],
     ]
 )
+send_4 = np.array(
+    [
+        [1882.0, 1310.2, 1638.0],
+        [1910.2, 1710.3, 1776.4],
+        [1624.6, 1365.4, 1434.5],
+        [1304.3, 1665.4, 1446.7],
+        [1033.8, 1037.5, 1538.6],
+        [957.3, 784.2, 1034.5],
+        [1051.5, 831.4, 767.7],
+        [852.5, 981.3, 1388.6],
+        [999.2, 767.0, 1022.6],
+        [367.5, 534.1, 777.3],
+        [596.7, 119.1, 479.5],
+    ]
+)
+recv_4 = np.array(
+    [
+        [2176.5, 1310.2, 1573.4],
+        [2139.7, 1820.7, 1776.4],
+        [1842.0, 1584.2, 1653.1],
+        [1522.4, 2014.4, 1847.6],
+        [1068.9, 1236.2, 1684.1],
+        [1177.3, 1026.2, 1123.5],
+        [1177.8, 994.8, 897.6],
+        [979.5, 1185.3, 1412.6],
+        [1078.3, 840.2, 1089.0],
+        [575.7, 658.6, 894.6],
+        [601.3, 218.3, 593.7],
+    ]
+)
 
-send_speed_1_average = np.array([np.mean(lst) for lst in send_speed_1])
-recv_speed_1_average = np.array([np.mean(lst) for lst in recv_speed_1])
-send_speed_2_average = np.array([np.mean(lst) for lst in send_speed_2])
-recv_speed_2_average = np.array([np.mean(lst) for lst in recv_speed_2])
+send_2_average = np.array([np.mean(lst) for lst in send_2])
+recv_2_average = np.array([np.mean(lst) for lst in recv_2])
+send_3_average = np.array([np.mean(lst) for lst in send_3])
+recv_3_average = np.array([np.mean(lst) for lst in recv_3])
+send_4_average = np.array([np.mean(lst) for lst in send_4])
+recv_4_average = np.array([np.mean(lst) for lst in recv_4])
 
-fig, axes = plt.subplots(1, 2)
+fig, axes = plt.subplots(1, 3)
 
-plt.subplot(1, 2, 1)
-plt.title("No Switch")
+plt.subplot(1, 3, 1)
+plt.title("Stage 2")
 plt.xlim(0, 1)
 plt.ylim(0, 5000)
-plt.plot(ber, send_speed_1_average, label="Send")
-plt.plot(ber, recv_speed_1_average, label="Receive")
+plt.plot(ber, send_2_average, label="Send")
+plt.plot(ber, recv_2_average, label="Receive")
 plt.legend()
 
-plt.subplot(1, 2, 2)
-plt.title("With Switch")
+plt.subplot(1, 3, 2)
+plt.title("Stage 3")
 plt.xlim(0, 1)
 plt.ylim(0, 5000)
-plt.plot(ber, send_speed_2_average, label="Send")
-plt.plot(ber, recv_speed_2_average, label="Receive")
+plt.plot(ber, send_3_average, label="Send")
+plt.plot(ber, recv_3_average, label="Receive")
+plt.legend()
+
+plt.subplot(1, 3, 3)
+plt.title("Stage 4")
+plt.xlim(0, 1)
+plt.ylim(0, 5000)
+plt.plot(ber, send_4_average, label="Send")
+plt.plot(ber, recv_4_average, label="Receive")
 plt.legend()
 
 fig.add_subplot(111, frameon=False)
